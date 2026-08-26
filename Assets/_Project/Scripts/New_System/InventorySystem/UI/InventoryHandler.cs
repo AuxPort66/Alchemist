a14ce37faf12387bb4bfdf58f4d9109872314c00
Sara <93783740+AuxPort66@users.noreply.github.com>
2026-08-26 03:19:23 +0200

    feat: Drawer close/open with drag feel

--- Assets/_Project/Scripts/New_System/InventorySystem/UI/InventoryHandler.cs
+++ Assets/_Project/Scripts/New_System/InventorySystem/UI/InventoryHandler.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InventoryHandler : MonoBehaviour, IPointerClickHandler
+public class InventoryHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [Header("Drawer")]
     public RectTransform drawer;
@@ -17,12 +17,33 @@ public class InventoryHandler : MonoBehaviour, IPointerClickHandler
     public bool isOpen = true;
     public bool isAnimating = false;
 
-    public void OnPointerClick(PointerEventData eventData)
+    [Header("Gesture")]
+    public float dragThreshold;
+    public Vector2 dragStartPos;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        if (!isAnimating)
-        {
-            StartCoroutine(ToggleDrawer());
-        }
+        if (isAnimating) return;
+        dragStartPos = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isAnimating) return;
+
+        float deltaX = eventData.position.x - dragStartPos.x;
+
+        if (Mathf.Abs(deltaX) < dragThreshold) return;
+        if (deltaX < 0 && isOpen) return;
+        else if (deltaX > 0 && !isOpen) return;
+
+        StartCoroutine(ToggleDrawer());
+        eventData.pointerDrag = null;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+       
     }
 
     private IEnumerator ToggleDrawer()
@@ -39,7 +60,7 @@ public class InventoryHandler : MonoBehaviour, IPointerClickHandler
             t += Time.deltaTime;
 
             float curveValue = curve.Evaluate(t / duration);
-            drawer.anchoredPosition = Vector2.Lerp(start, target, curveValue);
+            drawer.anchoredPosition = Vector2.LerpUnclamped(start, target, curveValue);
             yield return null;
         }
 
